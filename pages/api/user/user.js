2557ae1b42e47a56f148ff42be6ec17cb2f721aa
BingChills <6438147621@student.chula.ac.th>
2025-01-20 00:25:46 +0700

    Debeug2

--- pages/api/user/user.js
+++ pages/api/user/user.js
@@ -1,42 +1,42 @@
-import qs from "qs";
-import axios from "axios";
+// import qs from "qs";
+// import axios from "axios";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { idToken } = req.body;
+// export default async function handler(req, res) {
+//   if (req.method === "POST") {
+//     const { idToken } = req.body;
 
-    const postData = {
-      id_token: idToken,
-      client_id: "2006705425", // Channel ID from console
-    };
+//     const postData = {
+//       id_token: idToken,
+//       client_id: "2006705425", // Channel ID from console
+//     };
 
-    try {
-      const response = await axios.post(
-        "https://api.line.me/oauth2/v2.1/verify",
-        qs.stringify(postData),
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
+//     try {
+//       const response = await axios.post(
+//         "https://api.line.me/oauth2/v2.1/verify",
+//         qs.stringify(postData),
+//         {
+//           headers: {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//           },
+//         }
+//       );
 
-      const decodedToken = response.data;
+//       const decodedToken = response.data;
 
-      if (!decodedToken) {
-        return res.status(400).send("Invalid token");
-      }
+//       if (!decodedToken) {
+//         return res.status(400).send("Invalid token");
+//       }
 
-      res.status(200).json(decodedToken);
-    } catch (error) {
-      console.error(
-        "Error verifying token:",
-        error.response ? error.response.data : error.message
-      );
-      res.status(400).send("Invalid token");
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+//       res.status(200).json(decodedToken);
+//     } catch (error) {
+//       console.error(
+//         "Error verifying token:",
+//         error.response ? error.response.data : error.message
+//       );
+//       res.status(400).send("Invalid token");
+//     }
+//   } else {
+//     res.setHeader("Allow", ["POST"]);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
