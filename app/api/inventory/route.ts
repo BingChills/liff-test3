fd49a344f074ba91139c12ede483c9a65b4a43bb
BingChills <6438147621@student.chula.ac.th>
2025-01-24 11:56:16 +0700

    Remake

--- app/api/inventory/route.ts
+++
@@ -1,3 +0,0 @@
-// get user's inventory
-// return all pets that the user has and current equipped pets
-// object: {equipped: [pet1, pet2, pet3], inventory: [pet4, pet5, pet6]}
--- app/api/openBlindBox/route.ts
+++
@@ -1,5 +0,0 @@
-// Post request
-// Minus user's point to open a blind box
-// Randomly select a prize
-// Update user's inventory (database)
-// Return the prize to the user
