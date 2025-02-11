145470d3324bc379762f5875da040089b594a204
Putter-Ravit <putterbcc@gmail.com>
2025-02-11 15:53:43 +0700

    Fix bugs and and add bg music and sound effect

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -3,9 +3,10 @@ import { EventBus } from "../EventBus";
 
 export class Game extends Scene {
     private player!: Phaser.Physics.Arcade.Sprite;
-    private chests!: Phaser.Physics.Arcade.Group; // Group to manage all chests
+    private chests!: Phaser.Physics.Arcade.Group;
     private score: number = 0;
     private scoreText!: Phaser.GameObjects.Text;
+    private backgroundMusic!: Phaser.Sound.BaseSound;
 
     constructor() {
         super("Game");
@@ -17,11 +18,20 @@ export class Game extends Scene {
         this.load.image("corgi", "corgi.png");
         this.load.image("chest", "chest.png");
         this.load.image("background", "grass_bg.png");
+        this.load.audio("bgMusic", ["8-bit-arcade.mp3"]);
+        this.load.audio("collectSound", ["collect.mp3"]);
     }
 
     create() {
         // Add background
         this.add.image(430 / 2, 932 / 2, "background");
+
+        // Add and play background music
+        this.backgroundMusic = this.sound.add("bgMusic", {
+            volume: 0.5,
+            loop: true,
+        });
+        this.backgroundMusic.play();
 
         // Add player sprite
         this.player = this.physics.add
@@ -29,14 +39,13 @@ export class Game extends Scene {
             .setScale(0.08);
 
         // Create a group for chests
-        // NOTE: make more starts and make respawn system
         this.chests = this.physics.add.group();
 
         // Spawn 4 chest at random positions
         for (let i = 0; i < 4; i++) {
             const chest = this.chests.create(
-                Phaser.Math.Between(20, 410), // X Position
-                Phaser.Math.Between(20, 912), // Y Position
+                Phaser.Math.Between(20, 410),
+                Phaser.Math.Between(20, 912),
                 "chest"
             );
             chest.setScale(0.08);
@@ -45,23 +54,23 @@ export class Game extends Scene {
         // Add score text
         this.scoreText = this.add.text(25, 50, "Score: 0", {
             fontSize: "24px",
-            fontStyle: "bold", // Makes the text bold
-            color: "#000", // Black fill color
-            stroke: "#fff", // White outline color
-            strokeThickness: 4, // Thickness of the outline
+            fontStyle: "bold",
+            color: "#000",
+            stroke: "#fff",
+            strokeThickness: 4,
         });
 
         // Enable collision detection between player and chest
         this.physics.add.overlap(
             this.player,
             this.chests,
-            this.collectChest,
+            this
+                .collectChest as Phaser.Types.Physics.Arcade.ArcadePhysicsCallback,
             undefined,
             this
         );
 
         // Input listener for movement
-        // NOTE: don't quite understand
         this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
             this.physics.moveTo(this.player, pointer.x, pointer.y, 150);
         });
@@ -69,8 +78,6 @@ export class Game extends Scene {
     }
 
     update() {
-        // Stop player movement when close to the destination
-        // NOTE: still buggy
         if (
             Phaser.Math.Distance.Between(
                 this.player.x,
@@ -84,27 +91,23 @@ export class Game extends Scene {
     }
 
     collectChest = (
-        player:
-            | Phaser.Types.Physics.Arcade.GameObjectWithBody
-            | Phaser.Tilemaps.Tile,
-        chest:
-            | Phaser.Types.Physics.Arcade.GameObjectWithBody
-            | Phaser.Tilemaps.Tile
+        object1: Phaser.GameObjects.GameObject,
+        object2: Phaser.GameObjects.GameObject
     ) => {
-        // Since we know these are Sprites, we can safely cast them
-        const chestSprite = chest as Phaser.Physics.Arcade.Sprite;
-
-        // Destroy the collected chest
+        const chestSprite = object2 as Phaser.Physics.Arcade.Sprite;
         chestSprite.destroy();
 
-        // Increase the score and update the text
+        // Play collection sound
+        this.sound.play("collectSound", {
+            volume: 0.5,
+        });
+
         this.score += 10;
         this.scoreText.setText("Score: " + this.score);
 
-        // Spawn a new chest at a random position
         const newChest = this.chests.create(
-            Phaser.Math.Between(40, 410), // X position
-            Phaser.Math.Between(40, 912), // Y position
+            Phaser.Math.Between(40, 410),
+            Phaser.Math.Between(40, 912),
             "chest"
         );
         newChest.setScale(0.08);
